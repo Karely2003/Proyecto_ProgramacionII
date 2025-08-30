3efe663c493f5189fe480fa75c9e93ac15727cba
Janeri98 <janeri_juanez@unitec.edu>
2025-08-29 18:16:51 -0600

    TercerComit

---
+++ proyecto/src/pages/Citas.tsx
@@ -0,0 +1,9 @@
+// src/pages/Citas.tsx
+export default function Citas() {
+  return (
+    <div className="page-content">
+      <h2>Citas</h2>
+      <p>Gestion de citas</p>
+    </div>
+  );
+}
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-console.log("Hola Mundo")
-console.log("Hola SOY JANERI")
-console.log("hola soy yo de nuevo")
